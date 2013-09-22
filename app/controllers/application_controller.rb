9a819ea2663d90b2a4ef00e9689914ea73bda207
Steven Ko <chunyuko85@gmail.com>
2013-09-22 12:30:25 +0800

    change locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-
   helper_method :current_user, :logged_in?
+
+  before_action :set_locale
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -31,6 +31,25 @@ class ApplicationController < ActionController::Base
     else
       @cart_items = []
     end
+  end
+
+  def default_url_options(options={})
+    logger.debug "default_url_options is passed options: #{options.inspect}\n"
+    { locale: I18n.locale }
+  end
+
+private
+  
+  def set_locale
+    if params[:locale] && ["en", "zh-TW"].include?( params[:locale] )
+      I18n.locale = params[:locale] || I18n.default_locale
+    else
+      I18n.locale = extract_locale_from_accept_language_header
+    end
+  end
+
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
   end
   
 end
