b4d78c70cd74072a81dac3e4793e1d2b4492d383
Steven Ko <chunyuko85@gmail.com>
2013-09-27 20:35:40 +0800

    ckeditor

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,6 @@ Bundler.require(:default, Rails.env)
 
 module Hanchor
   class Application < Rails::Application
-
-    config.assets.precompile += Ckeditor.assets
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
