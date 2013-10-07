e027737c64a7f25a1564220fd1751346059452d5
Steven Ko <chunyuko85@gmail.com>
2013-10-07 10:48:55 +0800

    news locale

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,11 +1,11 @@
 class NewsController < ApplicationController
 
   def index
-    @news = News.paginate(:page => params[:page], :per_page => 3)
+    @news = News.locale(params[:locale]).paginate(:page => params[:page], :per_page => 5)
   end
 
   def show
-    @recent_news = News.limit(4)
+    @recent_news = News.locale(params[:locale]).limit(5)
     @news = News.find(params[:id])
   end
 
--- app/models/news.rb
+++ app/models/news.rb
@@ -1,3 +1,14 @@
 class News < ActiveRecord::Base
   mount_uploader :pic, ImageUploader
+
+  default_scope {order('sort DESC')}
+  
+  scope :locale, lambda { |locale|
+    if locale == "zh-TW"
+      where(is_tw: true)
+    else
+      where(is_en: true)
+    end
+  }
+
 end
