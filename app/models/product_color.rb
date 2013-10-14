8d5584a738caf864510a3de05083a4bf39363ceb
Steven Ko <chunyuko85@gmail.com>
2013-10-14 16:01:47 +0800

    en color size locale

--- app/models/product_color.rb
+++ app/models/product_color.rb
@@ -1,4 +1,8 @@
 class ProductColor < ActiveRecord::Base
   belongs_to :product
   validates_presence_of :color,:color_en
+
+  def locale_color language
+    ["zh-TW","zh"].include?( language )? color : color_en
+  end
 end
--- app/models/product_size.rb
+++ app/models/product_size.rb
@@ -1,4 +1,8 @@
 class ProductSize < ActiveRecord::Base
   belongs_to :product
   validates_presence_of :size,:size_en
+
+  def locale_size language
+    ["zh-TW","zh"].include?( language )? size : size_en
+  end
 end
