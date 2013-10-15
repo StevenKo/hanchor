25144bc5587e14b46c232d7dbd6222b4cc5e80e7
Steven Ko <chunyuko85@gmail.com>
2013-10-15 23:56:44 +0800

    user create

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,11 @@ class User < ActiveRecord::Base
   has_many :orders
 
   has_secure_password validations: false
-  validates :password, presence: true, on: :create, length: {minimum: 6}
+  validates :password, presence: true, on: :create, length: {minimum: 5}
+  validates :password, on: :update, length: {minimum: 5}, allow_blank: true
   validates_confirmation_of :password
   validates_presence_of :name,:email, :phone, :zip_code, :country
-  # validates_uniqueness_of :email, :case_sensitive => false
+  validates_uniqueness_of :email, :case_sensitive => false
 
   def admin?
     self.role == "admin"
