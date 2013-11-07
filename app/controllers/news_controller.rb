52dda75eb69b22ac35fef41f857483350aa4493d
Steven Ko <chunyuko85@gmail.com>
2013-11-07 21:33:38 +0800

    news fix

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -2,15 +2,16 @@ class NewsController < ApplicationController
 
   def index
     @news = News.locale(params[:locale]).paginate(:page => params[:page], :per_page => 5)
+    @recent_news = News.locale(params[:locale]).select("news.id, title, created_at").limit(5)
   end
 
   def show
-    @recent_news = News.locale(params[:locale]).limit(5)
+    @recent_news = News.locale(params[:locale]).select("news.id, title, created_at").limit(5)
     @news = News.find(params[:id])
   end
 
   def list
-    @recent_news = News.locale(params[:locale]).limit(5)
+    @recent_news = News.locale(params[:locale]).select("news.id, title, created_at").limit(5)
     if params[:news_tag_id].present?
       news_tag = NewsTag.find(params[:news_tag_id])
       @news = news_tag.news.locale(params[:locale]).select("news.id, title, created_at")
