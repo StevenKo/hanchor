323b3e52399788ba5287fa726e1df24cb3547019
Steven Ko <chunyuko85@gmail.com>
2015-04-29 10:44:28 +0800

    each page 16 news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,7 +1,7 @@
 class NewsController < ApplicationController
 
   def index
-    @news = News.locale(params[:locale]).select("news.id, title, release_date,pic").paginate(:page => params[:page], :per_page => 10).order("release_date DESC")
+    @news = News.locale(params[:locale]).select("news.id, title, release_date,pic").paginate(:page => params[:page], :per_page => 16).order("release_date DESC")
     @tags_selector = NewsTag.all.map{ |tag| [tag.locale(params[:locale]),tag.id]}
     respond_to do |format|
       format.html
@@ -18,9 +18,9 @@ class NewsController < ApplicationController
     @tags_selector = NewsTag.all.map{ |tag| [tag.locale(params[:locale]),tag.id]}
     if params[:news_tag_id].present? && params[:news_tag_id] != "0"
       news_tag = NewsTag.find(params[:news_tag_id])
-      @news = news_tag.news.locale(params[:locale]).select("news.id, title, release_date,pic").paginate(:page => params[:page], :per_page => 10).order("news.release_date DESC")
+      @news = news_tag.news.locale(params[:locale]).select("news.id, title, release_date,pic").paginate(:page => params[:page], :per_page => 16).order("news.release_date DESC")
     else
-      @news = News.locale(params[:locale]).select("id, title, release_date,pic").paginate(:page => params[:page], :per_page => 10).order("news.release_date DESC")
+      @news = News.locale(params[:locale]).select("id, title, release_date,pic").paginate(:page => params[:page], :per_page => 16).order("news.release_date DESC")
     end
 
     render "index"
