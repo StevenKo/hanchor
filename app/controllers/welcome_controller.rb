4a4fcb1d305c05e51a109b09af27c23739b607fa
Steven Ko <chunyuko85@gmail.com>
2015-10-14 14:15:56 +0800

    index products

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
   def index
     @news = News.locale(params[:locale]).select("news.id, title, release_date").limit(5).order("news.release_date DESC")
     @videos = Video.limit(2)
-    @products = Product.includes(:thumb).joins(:product_infos).where("product_infos.country_id = #{@country_id} and is_show_at_index = true").order_by_views_and_sort.select_info.limit(8)
+    @products = Product.includes(:thumb).joins(:product_infos).where("product_infos.country_id = #{@country_id} and is_show_at_index = true").order_by_views_and_sort.select_info
     @banners = Banner.order("sort DESC")
   end
 
